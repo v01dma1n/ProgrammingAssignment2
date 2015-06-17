59692e564a6cab0147049b362047cd2b978825a1
v01dma1n <irek@post.com>
2015-06-16 20:56:51 -0400

    Functional code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Check this out: http://masterr.org/r/how-to-cache-a-matrix-inversion-in-r/
 
-## Write a short comment describing this function
-
+## Create vector of get/set functions for input matrix and imverted matrices
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y    # remember new matrix
+    i <<- NULL # invalidate inversed one
+  }
+  get <- function() x # retrieve matrix
+  setsolve <- function(solve) i <<- solve
+  getsolve <- function() i
+  
+  list(set = set, 
+       get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## Return a matrix that is the inverse of 'xx'
+cacheSolve <- function(xx, ...) {
+  
+  s <- xx$getsolve() # do we have cached solution?
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  else {
+    data = xx$get()
+    s = solve(data, ...)
+    xx$setsolve(s) # remember solved matrix
+    return(s)
+  }
 
 }
 
 
-## Write a short comment describing this function
+if(exists("mat"))
+  rm(mat)
+mat <- matrix(rnorm(9), ncol=3, nrow=3)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+mv <- makeCacheMatrix(mat)
+cacheSolve(mv)
+cacheSolve(mv)
