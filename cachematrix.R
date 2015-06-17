2fe5083bfcd7a449c38eba2545919e72392590ca
v01dma1n <irek@post.com>
2015-06-16 21:05:52 -0400

    Commented solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Check this out: http://masterr.org/r/how-to-cache-a-matrix-inversion-in-r/
 
 ## Create vector of get/set functions for input matrix and imverted matrices
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
+# Build a list of functions for access to
+#   set() - assing matrix
+#   get() - retrieve matrix
+#   setsolve() - assign inversion
+#   getsolve() - retrieve inversion
+  
+  i <- NULL # no inverted matrix
   
   set <- function(y) {
     x <<- y    # remember new matrix
-    i <<- NULL # invalidate inversed one
+    i <<- NULL # invalidate inverted one
   }
-  get <- function() x # retrieve matrix
-  setsolve <- function(solve) i <<- solve
+  get <- function() x 
+  setsolve <- function(solve) i <<- solve 
   getsolve <- function() i
   
   list(set = set, 
@@ -22,25 +27,27 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Return a matrix that is the inverse of 'xx'
 cacheSolve <- function(xx, ...) {
   
-  s <- xx$getsolve() # do we have cached solution?
-  if(!is.null(s)) {
+  s <- xx$getsolve() 
+  if(!is.null(s)) { # do we have cached inverted matrix?
     message("getting cached data")
-    return(s)
+    return(s) # yes, return cached invsed matrix
   }
-  else {
+  else { # calculate inversion now
     data = xx$get()
     s = solve(data, ...)
-    xx$setsolve(s) # remember solved matrix
-    return(s)
+    xx$setsolve(s) # remember inverted matrix
+    return(s) # return calculated inverted matrix
   }
 
 }
 
-
-if(exists("mat"))
-  rm(mat)
-mat <- matrix(rnorm(9), ncol=3, nrow=3)
-
-mv <- makeCacheMatrix(mat)
-cacheSolve(mv)
-cacheSolve(mv)
+# Test code
+# if(exists("mat"))
+#   rm(mat)
+# mat <- matrix(rnorm(9), ncol=3, nrow=3)
+# 
+# mv <- makeCacheMatrix(mat)
+# # First calculate and cache
+# cacheSolve(mv)
+# # Now use the cache. Expecting message "getting cached data"
+# cacheSolve(mv)
